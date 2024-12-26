0784f2ed05cceac35972799c5b631e0ff6db73c0
Nenad <nesaeco@gmail.com>
2024-12-26 15:47:03 +0100

    Add suspense import

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 import '@styles/globals.css'
+import { Suspense } from 'react'
 
 export const metadata = {
   title: 'Promptopia',
